9e569f8e8ea4b0b8b46f55e69751e90855967ac1
roushan-sheik <roushansheik@gmail.com>
2024-02-19 07:23:50 +0600

    subtract the seat count added

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log("Hello world");
+// Total seat counter
+const seatCounters = document.getElementById("total_seat");
+let seatCount = parseInt(seatCounters.innerText);
+console.log(seatCount);
+// Get all seats button
+const allSetas = document.getElementsByClassName("kbd");
+// ============== Loop throw the all elements ====================
+for (const seat of allSetas) {
+  seat.addEventListener("click", (event) => {
+    //   subtract the seat count
+    seatCount -= 1;
+    // set update seat count value
+    seatCounters.innerText = seatCount;
+    console.log(event.target);
+  });
+}

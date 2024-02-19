9191ebda6dd77a699490f04c4b24d617059368ba
roushan-sheik <roushansheik@gmail.com>
2024-02-19 08:32:01 +0600

    Seat style updated

--- scripts/app.js
+++ scripts/app.js
@@ -5,13 +5,23 @@ let seatCount = parseInt(seatCounters.innerText);
 console.log(seatCount);
 // Get all seats button
 const allSetas = document.getElementsByClassName("kbd");
-// ============== Loop throw the all elements ====================
+//NOTE ============= Loop throw the all elements ====================
 for (const seat of allSetas) {
   seat.addEventListener("click", (event) => {
     //   subtract the seat count
     seatCount -= 1;
     // set update seat count value
     seatCounters.innerText = seatCount;
+    // change the button color
+    chanBtnColor(event.target);
     console.log(event.target);
   });
+}
+//NOTE - Chang the button color function
+function chanBtnColor(target) {
+  console.log(target.parentNode.childNodes);
+  target.parentNode.childNodes[1].classList.add("bg-green-primary");
+ 
+  console.log(target);
+  console.log("clicked");
 }

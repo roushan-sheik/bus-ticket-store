f77b34f7bda6dfd278637bc72b07f9e266d02f8a
roushan-sheik <roushansheik@gmail.com>
2024-02-19 10:06:00 +0600

    You cannot buy more than 4 tickets

--- scripts/app.js
+++ scripts/app.js
@@ -9,19 +9,32 @@ let totalSeat = parseInt(totaSeats.innerText);
 const allSetas = document.getElementsByClassName("seat");
 
 //NOTE ============= Loop throw the all elements ====================
+const tempArr = [];
 for (const seat of allSetas) {
   seat.addEventListener("click", (event) => {
-    event.stopPropagation();
+    if (tempArr.length > 3) {
+      alert("You cannot buy more than 4 tickets!");
+      return;
+    }
     //   subtract the seat count
     totalSeat -= 1;
     seatCount += 1;
+
     // set update seat count value
     totaSeats.innerText = totalSeat;
     // update count
     seatCountsValue.innerText = seatCount;
     // change the button color
     chanBtnColor(event.target);
+    // append the seat to the list
+    if (event.target.nodeName == "SPAN" || event.target.nodeName == "DIV") {
+      tempArr.push(event.target);
+    }
   });
+}
+// create element function
+function createElement(tagName) {
+  return document.createElement(tagName);
 }
 //NOTE - Chang the button color function
 function chanBtnColor(target) {

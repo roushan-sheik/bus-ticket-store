4c34b0af209e8308d27fd83d2f0ca7093143385e
roushan-sheik <roushansheik@gmail.com>
2024-02-19 09:39:57 +0600

    button click total seat count ticket count added

--- scripts/app.js
+++ scripts/app.js
@@ -1,27 +1,36 @@
 "use strict";
 // Total seat counter
-const seatCounters = document.getElementById("total_seat");
-let seatCount = parseInt(seatCounters.innerText);
-console.log(seatCount);
+const totaSeats = document.getElementById("total_seat");
+const seatCountsValue = document.getElementById("seat_count");
+let seatCount = parseInt(seatCountsValue.innerText);
+let totalSeat = parseInt(totaSeats.innerText);
+
 // Get all seats button
-const allSetas = document.getElementsByClassName("kbd");
+const allSetas = document.getElementsByClassName("seat");
+
 //NOTE ============= Loop throw the all elements ====================
 for (const seat of allSetas) {
   seat.addEventListener("click", (event) => {
+    event.stopPropagation();
     //   subtract the seat count
-    seatCount -= 1;
+    totalSeat -= 1;
+    seatCount += 1;
     // set update seat count value
-    seatCounters.innerText = seatCount;
+    totaSeats.innerText = totalSeat;
+    // update count
+    seatCountsValue.innerText = seatCount;
     // change the button color
-    chanBtnColor(event.target);
-    console.log(event.target);
+    if (event.target.nodeName == "DIV" || event.target.nodeName == "SPAN") {
+      chanBtnColor(event.target);
+    }
   });
 }
 //NOTE - Chang the button color function
 function chanBtnColor(target) {
-  console.log(target.parentNode.childNodes);
-  target.parentNode.childNodes[1].classList.add("bg-green-primary");
- 
-  console.log(target);
-  console.log("clicked");
+  if (target.nodeName == "SPAN") {
+    target.parentNode.classList.add("bg_primary");
+    target.parentNode.classList.add("bg_primary");
+  } else if (target.nodeName == "DIV") {
+    target.classList.add("bg_primary");
+  }
 }
